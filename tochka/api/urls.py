18fc193cbb641cca2b54e8fae792aefd191d08aa
pribit <3korbandallas22@gmail.com>
2020-08-30 21:53:10 +0500

    Add /api/ping

--- tochka/api/urls.py
+++ tochka/api/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = format_suffix_patterns([
     path('account_list/', views.AccountViewSet.as_view({'get': 'list'})),
+    path('ping/', views.ping),
     path('status/<uuid:pk>', views.AccountViewSet.as_view({'get': 'retrieve'})),
     path('add/<uuid:pk>', views.AddSumToBalanceViewSet.as_view({'post': 'update'})),
     path('subtract/<uuid:pk>', views.SubtractBalanceViewSet.as_view({'post': 'update'})),
